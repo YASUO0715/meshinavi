e46ea484b894612d7a6c7648802629e33ce016c3
YASUO0715 <yasuo0715niihori@gmail.com>
2021-10-02 09:32:34 +0900

    bootstrap適用

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+{{-- 親レイアウト yield = 埋め込む --}}
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>めしナビ - @yield('title')</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
+
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+    
+</body>
+
+</html>
---
+++ resources/views/restaurants/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+@section('title', '一覧画面')
+@section('content')
+    @if (!empty($restaurants))
+        <ul>
+            @foreach ($restaurants as $restaurant)
+                <li class="list-unstyled border mb-5 pl-3 shadow">
+                    <a href="{{ route('restaurants.show', $restaurant) }}">{{ $restaurant->name }}</a>
+                </li>
+            @endforeach
+        </ul>
+    @endif
+@endsection
